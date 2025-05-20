28d5a0a3d93e1374a519906473a38b898d47f473
ThotaAkanksha <thotaakanksha04@gmail.com>
2025-05-20 17:18:24 +0530

    Add files via upload

---
+++ calculation.py
@@ -0,0 +1,8 @@
+import math
+num = float(input("Enter a number: "))
+sqrt = math.sqrt(num)
+log_num = math.log(num)
+sine_num = math.sin(num)
+print("The square root of ", num, "is", math.sqrt(num))
+print("natural logarithm of ", num, "is", log_num)
+print("The sine of ", num, "is", sine_num)
